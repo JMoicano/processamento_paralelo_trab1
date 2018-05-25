a1f5c7b62a39fbde2e98d15fc47823b5aabab71b
JMoicano <JMoicano@DESKTOP-8IM63T7>
2018-05-25 13:21:36 -0300

    código de decriptação sequencial

--- src/br/inf/ufes/ppd/Decrypt.java
+++ src/br/inf/ufes/ppd/Decrypt.java
@@ -4,13 +4,23 @@ package br.inf.ufes.ppd;
 
 
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.rmi.RemoteException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.*;
 
 public class Decrypt {
@@ -44,8 +54,54 @@ public class Decrypt {
 
 
 	public static void main(String[] args) {
-		// args[0] e a chave a ser usada
+		// args[0] arquivo dicionario
 		// args[1] e o nome do arquivo de entrada
+		// args[2] texto conhecido
+		File f = new File(args[0]);
+		File inFile = new File(args[1]);
+		String known_text = args[2];
+		List<String> _dict = null;
+		try(FileReader fileReader = new FileReader(f);
+			BufferedReader b = new BufferedReader(fileReader)) {
+			_dict = new ArrayList<String>();
+			String readLine = "";
+			while ((readLine = b.readLine()) != null) {
+				_dict.add(readLine);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		byte[] ciphertext = null;
+		
+		try {
+			ciphertext = Files.readAllBytes(inFile.toPath());
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		long startTime = System.nanoTime();		
+		for (int aux = 0; aux < _dict.size(); ++aux) {
+		
+			byte[] key = _dict.get((int)aux).getBytes();
+			try {
+				SecretKeySpec keySpec = new SecretKeySpec(key, "Blowfish");
+	
+				Cipher cipher = Cipher.getInstance("Blowfish");
+				cipher.init(Cipher.DECRYPT_MODE, keySpec);
+	
+				byte[] message = ciphertext;
+	
+				byte[] decrypted = cipher.doFinal(message);
+				String decryptedStr = new String(decrypted);
+				if (decryptedStr.contains(known_text)) {
+					saveFile(new String(key) +".msg", decrypted);
+				}
+			} catch (javax.crypto.BadPaddingException | InvalidKeyException | IllegalBlockSizeException | NoSuchAlgorithmException | NoSuchPaddingException | IOException e) { }
+			
+			
+		}
+		double time_diff = (System.nanoTime() - startTime)/1000000;
+		System.out.println(time_diff);
 
 		try {
 
@@ -60,7 +116,7 @@ public class Decrypt {
 
 			byte[] decrypted = cipher.doFinal(message);
 
-			saveFile(args[0]+".msg", decrypted);
+			
 
 		} catch (javax.crypto.BadPaddingException e) {
 			// essa excecao e jogada quando a senha esta incorreta
@@ -72,5 +128,7 @@ public class Decrypt {
 			e.printStackTrace();
 		}
 	}
+	
+	
 
 }
